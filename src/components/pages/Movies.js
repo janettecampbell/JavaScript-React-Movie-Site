96e810a4da1ee3d4deeae995c5ff30f90a3dccec
Janette Campbell <campbjp@hotmail.com>
2022-11-30 11:19:34 -0500

    feat: added load more button to Movie page

--- src/components/pages/Movies.js
+++ src/components/pages/Movies.js
@@ -4,6 +4,7 @@ import MovieList from "../layout/MovieList";
 
 const Movies = () => {
   const [popularMovies, setPopularMovies] = useState(null);
+  const [page, setPage] = useState(2);
 
   useEffect(() => {
     fetch(
@@ -13,11 +14,26 @@ const Movies = () => {
       .then((json) => setPopularMovies(json.results));
   }, []);
 
+  const handleClick = () => {
+    setPage(page + 1);
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=4af29920e903cef08f533ae3feff4860&language=en-US&page=${page}`
+    )
+      .then((res) => res.json())
+      .then((json) => setPopularMovies([...popularMovies, ...json.results]));
+  };
+
   return (
     <div className="movie-home">
       <NavBar />
       <div className="movie-list">
         {popularMovies && <MovieList popularMovies={popularMovies} />}
+      </div>
+      <div className="load-more">
+        <button className="btn-load" onClick={handleClick}>
+          Load More
+        </button>
       </div>
     </div>
   );
