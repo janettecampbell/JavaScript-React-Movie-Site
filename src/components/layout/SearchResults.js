91e3c8a9fcf5debf15b908ed900037b1ff5c1eb1
Janette Campbell <campbjp@hotmail.com>
2022-12-05 18:19:01 -0500

    chore: added comment

--- src/components/layout/SearchResults.js
+++ src/components/layout/SearchResults.js
@@ -6,6 +6,7 @@ const SearchResults = (props) => {
   return (
     <div className="movie-list">
       <div className="movie-items">
+        {/* get and render movie card for each movie */}
         {movieResults.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
