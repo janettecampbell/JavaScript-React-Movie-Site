ad49905045402448e5e19a1d06041a65fd7742ea
Janette Campbell <campbjp@hotmail.com>
2022-12-05 12:42:46 -0500

    chore: made load more btn visible

--- src/components/pages/TopRatedMovies.js
+++ src/components/pages/TopRatedMovies.js
@@ -4,15 +4,38 @@ import MovieList from "../layout/MovieList";
 
 const Movies = () => {
   const [topRatedMovies, setTopRatedMovies] = useState(null);
+  const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(2);
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/top_rated?api_key=4af29920e903cef08f533ae3feff4860&language=en-US&page=1"
-    )
-      .then((res) => res.json())
-      .then((json) => setTopRatedMovies(json.results));
-  }, []);
+    // reset search pages upon new search
+    setPage(2);
+
+    const fetchTopRatedData = async () => {
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=4af29920e903cef08f533ae3feff4860&language=en-US&page=1"
+      ).then((res) => res.json());
+
+      setTopRatedMovies(data.results);
+      setTotalPages(data.total_pages);
+    };
+
+    fetchTopRatedData();
+
+    // show load more button only if there are more search results
+    if (totalPages < page) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+
+    // fetch(
+    //   "https://api.themoviedb.org/3/movie/top_rated?api_key=4af29920e903cef08f533ae3feff4860&language=en-US&page=1"
+    // )
+    // .then((res) => res.json())
+    // .then((json) => setTopRatedMovies(json.results));
+  }, [totalPages]);
 
   const handleClick = () => {
     setPage(page + 1);
@@ -22,6 +45,13 @@ const Movies = () => {
     )
       .then((res) => res.json())
       .then((json) => setTopRatedMovies([...topRatedMovies, ...json.results]));
+
+    // show load more button only if there are more search results
+    if (totalPages < page + 1) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
   };
 
   return (
@@ -31,7 +61,10 @@ const Movies = () => {
         {topRatedMovies && <MovieList movies={topRatedMovies} />}
       </div>
       <div className="load-more">
-        <button className="btn-load" onClick={handleClick}>
+        <button
+          className={`btn-load${isVisible ? " btn-visible" : ""}`}
+          onClick={handleClick}
+        >
           Load More
         </button>
       </div>
